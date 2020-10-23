cef6374fabefba957a9b0736a86aeec443c102a3
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2020-10-23 21:08:56 +0200

    V0.1

--- example_config.sh
+++ example_config.sh
@@ -6,7 +6,7 @@ msg_start="Started printing $print_filename"
 msg_error="printing of $print_filename Failed"
 msg_pause="printing of $print_filename Paused"
 msg_end="Finished printing $print_filename"
-picture=1
+picture="1"
 webcam="http://127.0.0.1:8080/?action=snapshot"
 rotate="0"
 horizontally="0"
---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "\n\n========= moonraker-telegram - Installation Script ==========="
+echo "\n\n========= Creat telegram_config.sh ==========="
+
+cp -i /home/pi/moonraker-telegram/example_config.sh /home/pi/moonraker-telegram/telegram_config.sh
+cp -s /home/pi/moonraker-telegram/telegram_config.sh /home/pi/klipper_config/telegram_config.sh
+
+
+
+echo "\n\n========= set permissions ==========="
+sleep 1
+chmod 755 /home/pi/moonraker-telegram/telegram.sh
+chmod 755 /home/pi/moonraker-telegram/read_state.sh
+chmod 755 /home/pi/moonraker-telegram/telegram_config.sh
+
+echo "\n\n========= installation autostart ==========="
+
+crontab -u pi -l | grep -v "/home/pi/moonraker-telegram/read_state.sh  &"  | crontab -u pi -
+sleep 1
+(crontab -u pi -l ; echo "@reboot sh /home/pi/moonraker-telegram/read_state.sh  &") | crontab -u pi -
+
+echo "\n\n========= installation end ==========="
+echo "\n\n========= open and edit your config with ==========="
+echo "\n\n========= sudo nano /home/pi/moonraker-telegram/telegram_config.sh ==========="
+echo "\n\n========= or use mainsail and edit the telegram_config.sh ==========="
+
+
+exit 1
