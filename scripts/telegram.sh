a21d43cc4f5af39ad3601e17084773f5961227d4
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2020-10-23 20:27:59 +0200

    Update telegram.sh

--- scripts/telegram.sh
+++ scripts/telegram.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
+curl -s -o telegram_stats.txt http://127.0.0.1:$port/printer/objects/query?print_stats
+print_filename=$(grep -oP '(?<="filename": ")[^"]*' print_stats.txt)
+
 . /home/pi/moonraker-telegram/telegram_config.sh
 
 tokenurl="https://api.telegram.org/bot$token"
-msg="$1"
+state_msg="$1"
+
+if [ "$state_msg" = "1" ]; then
+    msg="$msg_start"
+
+elif [ "$state_msg" = "2" ]; then
+    msg="$msg_end"
+
+elif [ "$state_msg" = "2" ]; then
+    msg="$msg_pause"
+
+elif [ "$state_msg" = "2" ]; then
+    msg="$msg_error"
+fi
 
 if [ "$picture" = "1" ]; then
   wget -O /home/pi/moonraker-telegram/picture/cam_new.jpg $webcam
