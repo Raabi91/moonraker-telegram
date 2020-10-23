6db59309e34b4c106de405198f19bbadb6b297d1
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2020-10-23 19:39:32 +0200

    Update read_state.sh

--- scripts/read_state.sh
+++ scripts/read_state.sh
@@ -11,28 +11,28 @@ print_state_read=$(grep -oP '(?<="state": ")[^"]*' print_stats.txt)
 print_filename=$(grep -oP '(?<="filename": ")[^"]*' print_stats.txt)
 
 	if [ "$print_state_read" = "printing" ]; then
-        if [ "$print_state"="0" ]; then
-            msg="msg_start"
+        if [ "$print_state" = "0" ]; then
+            msg="$msg_start"
             print_state="1"
             sh /home/pi/moonraker-telegram/scripts/telegram.sh
         fi
 
     elif [ "$print_state_read" = "complete" ]; then
-	    if [ "$print_state"="1" ]; then
-            msg="msg_end"
+	    if [ "$print_state" = "1" ]; then
+            msg="$msg_end"
             print_state="0"
             sh /home/pi/moonraker-telegram/scripts/telegram.sh
         fi
 
     elif [ "$print_state_read" = "paused" ]; then
-        if [ "$print_state"="1" ]; then
-            msg="msg_pause"
+        if [ "$print_state" = "1" ]; then
+            msg="$msg_pause"
             sh /home/pi/moonraker-telegram/scripts/telegram.sh
         fi
     
     elif [ "$print_state_read" = "error" ]; then
-        if [ "$print_state"="1" ]; then
-	        msg="msg_error"
+        if [ "$print_state" = "1" ]; then
+	        msg="$msg_error"
             print_state="0"
             sh /home/pi/moonraker-telegram/scripts/telegram.sh
         fi
