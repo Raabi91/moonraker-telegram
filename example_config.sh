b050d0a84e297e2d3d1dfb195b738fb829599e70
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2020-10-23 18:59:26 +0200

    first try

---
+++ example_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+port="7125"
+token="493901798"
+chatid="493901798"
+msg_start="Started printing $print_filename"
+msg_error="printing of $print_filename Failed"
+msg_pause="printing of $print_filename Paused"
+msg_end="Finished printing $print_filename"
+picture=1
+webcam="http://127.0.0.1:8080/?action=snapshot"
---
+++ scripts/telegram.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. /home/pi/moonraker-telegram/telegram_config.sh
+
+tokenurl="https://api.telegram.org/bot$token"
+
+wget -O cam_new.jpg $webcam
+
+
+if [ "$picture" = "1" ]; then
+  wget -O /home/pi/moonraker-telegram/picture/cam_new.jpg $webcam
+  curl -s -X POST \
+    ${tokenurl}/sendPhoto \
+    -F chat_id=${chatid} \
+    -F photo="@/home/pi/moonraker-telegram/picture/cam_new.jpg" \
+    -F caption=""
+fi
+
+
+   curl -s -X POST \
+     ${tokenurl}/sendMessage \
+     -d text="$msg" \
+     -d chat_id=${chatid}
+
+exit
