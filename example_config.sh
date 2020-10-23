029855d1d4301bf77a9885ddb80187b073ba3e4b
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2020-10-23 19:06:39 +0200

    Update example_config.sh

--- example_config.sh
+++ example_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 port="7125"
-token="493901798"
-chatid="493901798"
+token="your_token"
+chatid="your_chatid"
 msg_start="Started printing $print_filename"
 msg_error="printing of $print_filename Failed"
 msg_pause="printing of $print_filename Paused"
