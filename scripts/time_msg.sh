003a468515f8fd5056144d66ced800ba739eb2c4
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2020-11-20 07:44:45 +0100

    Update time_msg.sh

--- scripts/time_msg.sh
+++ scripts/time_msg.sh
@@ -3,14 +3,24 @@
 while true
 do
 . /home/pi/moonraker-telegram/telegram_config.sh
+SECONDS=0
 
-sleep $time
+while [ $SECONDS -lt $time ]; do
+   . /home/pi/moonraker-telegram/scripts/time_config.txt
+      if [ "$time_msg" = "0" ]; then
+        exit 0 
+      fi
+sleep 10
+SECONDS=$(echo "$SECONDS + 10" | bc -l)
+    :
+done
 
 . /home/pi/moonraker-telegram/scripts/time_config.txt
 
 	if [ "$time_msg" = "1" ]; then
         if [ "$time_pause" = "0" ]; then
         sh /home/pi/moonraker-telegram/scripts/telegram.sh "5"
+        SECONDS=0
         fi
     fi
     if [ "$time_msg" = "0" ]; then
