4d37aa777bfc1fcf5934a8e22f9409211d863281
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2021-02-02 14:49:28 +0100

    new install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,15 +13,31 @@ pip3 install requests
 pip3 install telepot
 
 
-echo "\n\n========= Creat telegram_config.sh ==========="
+echo "\n\n========= Check for telegram_config.sh ==========="
+if [ -e $DIR/telegram_config.sh ]
+then 
+else
+    cp $DIR/example_config.sh $DIR/telegram_config.sh
+fi
 
-echo -e "\n\n========= pleas input your settings description on github ==========="
-echo -e "\n\nyour moonraker config path (like /home/pi/klipper_config):"
-read CONFIG
+if ! grep -q "config_dir=" $DIR/telegram_config.sh
+    then
+    echo -e "\n\n========= pleas input your settings description on github ==========="
+    echo -e "\n\nyour moonraker config path (like /home/pi/klipper_config):"
+    read CONFIG 
+    echo "# moonraker config path" >> $DIR/scripts/telegram_config.sh
+    echo "config_dir="$CONFIG"" >> $DIR/scripts/telegram_config.sh
+elif ! grep -q "bot_disable=" $DIR/telegram_config.sh
+    then 
+    echo "# Make all commands Disable with 1" >> $DIR/scripts/telegram_config.sh
+    echo "bot_disable="0"" >> $DIR/scripts/telegram_config.sh        
+fi
 
-cp -i $DIR/example_config.sh $DIR/telegram_config.sh
-sudo cp -s $DIR/telegram_config.sh $CONFIG/telegram_config.sh
 
+. $DIR/telegram_config.sh
+
+    sudo cp -s $DIR/telegram_config.sh $config_dir/telegram_config.sh
+    echo $config_dir/telegram_config.sh
 
 
 echo "\n\n========= set permissions ==========="
