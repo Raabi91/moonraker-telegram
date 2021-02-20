2a608576ba4e52c7c5aa25f5ebf37566b22bac5e
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2021-02-20 10:27:35 +0100

    first systemd test
    update install.sh. add .service and make a stop script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+MTPATH=$(sed 's/\/scripts//g' <<< $SCRIPTPATH)
+echo $SCRIPTPATH
 MYDIR=`dirname $0`
 DIR="`cd $MYDIR/../; pwd`"
 
@@ -24,8 +27,8 @@ fi
 
 if ! grep -q "config_dir=" $DIR/telegram_config.sh
     then
-    echo -e "\n\n========= pleas input your settings description on github ==========="
-    echo -e "\n\nyour moonraker config path (like /home/pi/klipper_config):"
+    echo -e "========= pleas input your settings description on github ==========="
+    echo -e "your moonraker config path (like /home/pi/klipper_config):"
     read CONFIG 
     echo '\n # moonraker config path' >> $DIR/telegram_config.sh
     echo "config_dir="$CONFIG"" >> $DIR/telegram_config.sh
@@ -35,9 +38,18 @@ if ! grep -q "bot_disable=" $DIR/telegram_config.sh
     echo '# Make all commands Disable with 1' >> $DIR/telegram_config.sh
     echo 'bot_disable="0"' >> $DIR/telegram_config.sh        
 fi
+if ! grep -q "multi_instanz=" $DIR/multi_config.sh
+    then 
+    echo -e "if you want to use multiple instances on one pi, enter an identifier here. this is needed to create the sytemd service"
+    echo -e "If you only use it once per hardware, simply press enter."
+    read INSTANZ 
+    echo '\n # if you want to use multiple instances on one pi, enter an identifier here. this is needed to create the sytemd service.' > $DIR/multi_config.sh
+    echo "multi_instanz="$INSTANZ"" >> $DIR/multi_config.sh      
+fi
 
 
 . $DIR/telegram_config.sh
+. $DIR/multi_config.sh
 
     cp -l $DIR/telegram_config.sh $config_dir/telegram_config.sh
 
@@ -54,14 +66,37 @@ sudo chmod 777 $DIR/telegram_config.sh
 
 echo "\n\n========= install autostart ==========="
 
+install_systemd_service()
+{
+    SERVICE=$(<$SCRIPTPATH/moonraker-telegram.service)
+    echo $SERVICE
+    MTPATH_ESC=$(sed "s/\//\\\\\//g" <<< $MTPATH)
+    echo $MTPATH_ESC
+
+    SERVICE=$(sed "s/MT_DESC/moonraker-telegram$multi_instanz/g" <<< $SERVICE)
+    echo $SERVICE
+    SERVICE=$(sed "s/MT_USER/$USER/g" <<< $SERVICE)
+    echo $SERVICE
+    SERVICE=$(sed "s/MT_DIR/$MTPATH_ESC/g" <<< $SERVICE)
+    echo $SERVICE
+
+    echo "$SERVICE" | sudo tee /etc/systemd/system/moonraker-telegram$multi_instanz.service > /dev/null
+    sudo systemctl daemon-reload
+    sudo systemctl enable moonraker-telegram$multi_instanz
+}
+
+
+start_moonraker-telegram() 
+{
+    sudo systemctl start moonraker-telegram$multi_instanz
+}
 crontab -u pi -l | grep -v "$DIR"  | crontab -u pi -
 sleep 1
-(crontab -u pi -l ; echo "@reboot sh $DIR/scripts/moonraker-telegram_start.sh &") | crontab -u pi -
+(crontab -u pi -l ; echo "") | crontab -u pi -
 
 echo "\n\n========= installation end ==========="
 echo "\n\n========= open and edit your config with ==========="
-echo "\n\n========= sudo nano $DIR/telegram_config.sh ==========="
-echo "\n\n========= or use mainsail or fluidd and edit the telegram_config.sh ==========="
+echo "\n\n========= mainsail or fluidd and edit the telegram_config.sh ==========="
 
 
 exit 1
