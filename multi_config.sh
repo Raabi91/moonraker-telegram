44babb004bd57682a225457c291eca5126b34963
Jens Raab <jensraab.jr@gmail.com>
2021-03-26 20:46:57 +0000

    update chmod

---
+++ multi_config.sh
@@ -0,0 +1,4 @@
+# moonraker config path
+config_dir=/home/pi/klipper_config
+# if you want to use multiple instances on one pi, enter an identifier here. this is needed to create the sytemd service.
+multi_instanz=moonraker-telegram
