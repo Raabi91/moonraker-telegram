888c0d916d960e6ab617bcfc91f5ff1ae81e7fc5
Jens Raab <jensraab.jr@gmail.com>
2021-05-13 09:13:55 +0100

    add gcode upload

--- scripts/bot.py
+++ scripts/bot.py
@@ -13,46 +13,60 @@ DIR = sys.argv[3]
 chatid_mt = sys.argv[4]
 
 def on_chat_message(msg):
-    chat_id = msg['chat']['id']
-    command = msg['text']
-    if "@" in command:
-        command, x = command.split("@")
-    print ('Got chat: %s' % chat_id)
-    print ('chat  id: %s' % chatid_mt)
-    print ('Got command: %s' % command)
-    
+    content_type, chat_type, chat_id = telepot.glance(msg,'chat')
+    print(content_type)
+    print(chat_id)
     if str(chat_id) != chatid_mt:
-        print ('chat id are not same')
-        bot.sendMessage(chat_id, 'you have no permission to communicate with me.')
+            print ('chat id are not same')
+            bot.sendMessage(chat_id, 'you have no permission to communicate with me.')
     else:
-        print ('chat id are same')
-        if command == '/help':
-            os.system(f'sh {DIR}/scripts/telegram.sh 6')
-        elif command == '/state':
-            os.system(f'sh {DIR}/scripts/telegram.sh 5')
-        elif command == '/print':
-            os.system(f'bash {DIR}/scripts/list_files.sh')
-        elif command == '/gcode_macro':
-            os.system(f'bash {DIR}/scripts/gcode.sh')
-        elif command == '/power':
-            os.system(f'bash {DIR}/scripts/power.sh')
-        elif command == '/pause':
-            content_type, chat_type, chat_id = telepot.glance(msg)
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                [InlineKeyboardButton(text='yes', callback_data='yes_pause'),
-                InlineKeyboardButton(text='no', callback_data='no')],
-                ])
-            bot.sendMessage(chat_id, 'do you really want to pause', reply_markup=keyboard)
-        elif command == '/resume':
-            x = requests.post(f'http://127.0.0.1:{port}/printer/print/resume')
-            print(x.text)
-        elif command == '/cancel':
-            content_type, chat_type, chat_id = telepot.glance(msg)
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                [InlineKeyboardButton(text='yes', callback_data='yes_cancel'),
-                InlineKeyboardButton(text='no', callback_data='no')],
-                ])
-            bot.sendMessage(chat_id, 'do you really want to abort', reply_markup=keyboard)
+        if content_type == 'document':
+            file_id = msg['document']['file_id']
+            file_name = msg['document']['file_name']
+            if file_name.endswith('.gcode'):
+                print(file_id)
+                print(file_name)
+                os.system(f'{DIR}/scripts/upload.sh "{file_id}" "{file_name}"')
+        else:
+            chat_id = msg['chat']['id']
+            command = msg['text']
+            if "@" in command:
+                command, x = command.split("@")
+            print ('Got chat: %s' % chat_id)
+            print ('chat  id: %s' % chatid_mt)
+            print ('Got command: %s' % command)
+            if str(chat_id) != chatid_mt:
+                print ('chat id are not same')
+                bot.sendMessage(chat_id, 'you have no permission to communicate with me.')
+            else:
+                print ('chat id are same')
+                if command == '/help':
+                    os.system(f'sh {DIR}/scripts/telegram.sh 6')
+                elif command == '/state':
+                    os.system(f'sh {DIR}/scripts/telegram.sh 5')
+                elif command == '/print':
+                    os.system(f'bash {DIR}/scripts/list_files.sh')
+                elif command == '/gcode_macro':
+                    os.system(f'bash {DIR}/scripts/gcode.sh')
+                elif command == '/power':
+                    os.system(f'bash {DIR}/scripts/power.sh')
+                elif command == '/pause':
+                    content_type, chat_type, chat_id = telepot.glance(msg)
+                    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                        [InlineKeyboardButton(text='yes', callback_data='yes_pause'),
+                        InlineKeyboardButton(text='no', callback_data='no')],
+                        ])
+                    bot.sendMessage(chat_id, 'do you really want to pause', reply_markup=keyboard)
+                elif command == '/resume':
+                    x = requests.post(f'http://127.0.0.1:{port}/printer/print/resume')
+                    print(x.text)
+                elif command == '/cancel':
+                    content_type, chat_type, chat_id = telepot.glance(msg)
+                    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                        [InlineKeyboardButton(text='yes', callback_data='yes_cancel'),
+                        InlineKeyboardButton(text='no', callback_data='no')],
+                        ])
+                    bot.sendMessage(chat_id, 'do you really want to abort', reply_markup=keyboard)
             
 def on_callback_query(msg):
     print('msg:', msg)
