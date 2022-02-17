295ffd877a349c03f1fd39720dac953071f7392a
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2022-02-17 16:37:49 +0100

    Update bot.py

--- scripts/bot.py
+++ scripts/bot.py
@@ -61,8 +61,8 @@ def on_chat_message(msg):
                     content_type, chat_type, chat_id = telepot.glance(msg)
                     keyboard = InlineKeyboardMarkup(inline_keyboard=[
                         [InlineKeyboardButton(text='Firmware Restart', callback_data='fi_re'),
-                         InlineKeyboardButton(text='Klipper Restart', callback_data='ki_re'),
-                         InlineKeyboardButton(text='reboot Host', callback_data='pi_re'),
+                         InlineKeyboardButton(text='Klipper Restart', callback_data='ki_re')]
+                        [InlineKeyboardButton(text='reboot Host', callback_data='pi_re'),
                          InlineKeyboardButton(text='shutdown Host', callback_data='pi_sh')],
                     ])
                     bot.sendMessage(
