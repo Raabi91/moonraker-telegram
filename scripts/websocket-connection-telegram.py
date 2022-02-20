e2c265946f08e1694460d93c8d8aa8242e53e18e
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2022-02-20 13:03:01 +0100

    Update websocket-connection-telegram.py

--- scripts/websocket-connection-telegram.py
+++ scripts/websocket-connection-telegram.py
@@ -79,9 +79,16 @@ def on_message(ws, message):
     if "print_stats" in message:
         if "state" in message:
             timelapse = requests.get(f'http://127.0.0.1:{port}/printer/objects/query?gcode_macro%20TIMELAPSE_TAKE_FRAME', headers={"X-Api-Key":f'{api_key}'}).json()
-            obj = json.dumps(timelapse)
-            obj1 = json.loads(obj)
-            if obj1["result"]["status"]["gcode_macro TIMELAPSE_TAKE_FRAME"]["is_paused"] == False :
+            if "is_paused" in str(timelapse):
+                obj = json.dumps(timelapse)
+                obj1 = json.loads(obj)
+                if obj1["result"]["status"]["gcode_macro TIMELAPSE_TAKE_FRAME"]["is_paused"] == False :
+                    print(message)
+                    f = open(f'{DIR1}/websocket_state.txt', 'w')
+                    f.write(message)
+                    f.close()
+                    os.system(f'bash {DIR1}/scripts/read_state.sh "0"')
+            else
                 print(message)
                 f = open(f'{DIR1}/websocket_state.txt', 'w')
                 f.write(message)
