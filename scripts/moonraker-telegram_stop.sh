73494893527f312fdda979e2b9881b0ea932d852
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2022-03-05 10:11:57 +0100

    switch to bash for start without new install

--- scripts/moonraker-telegram_stop.sh
+++ scripts/moonraker-telegram_stop.sh
@@ -2,16 +2,5 @@
 MYDIR_STOP=`dirname $0`
 DIR_STOP="`cd $MYDIR_STOP/../; pwd`"
 
-. $DIR_STOP/multi_config.sh
-. $DIR_STOP/example_config.sh
-. $config_dir/telegram_config.conf
-
-log=/tmp/$multi_instanz.log
-echo "$(date)" >> $log
-echo "Stop $multi_instanz" >> $log
-echo "" >> $log
-echo "" >> $log
-
-
-ps aux | grep -ie $DIR_STOP | awk '{print $2}' | xargs kill -9
+bash $DIR_START/scripts/start.sh
 exit 1
